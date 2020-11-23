741ac63a8f1ee946e750a2cabc9f528aae1b3ea0
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-23 20:00:36 -0300

    Conecting to API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,42 +7,60 @@ import logoImg from '../../assets/logoImg.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
-  // CODE
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const res = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = res.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
 
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer 2" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/42852997?s=460&u=80a399122290571393cce240b45a9c53035750f3&v=4"
-            alt="Eduardo Ribeiro"
-          />
-          <div>
-            <strong>dufellipeR/github_explorer2</strong>
-            <p>Lorem ipsum dolor firelight bluetooth</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/42852997?s=460&u=80a399122290571393cce240b45a9c53035750f3&v=4"
-            alt="Eduardo Ribeiro"
-          />
-          <div>
-            <strong>dufellipeR/github_explorer2</strong>
-            <p>Lorem ipsum dolor firelight bluetooth</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
