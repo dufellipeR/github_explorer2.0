f2e467794f39a84f3bd68c926c0355a67696edbf
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-23 19:29:37 -0300

    Styling API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,19 +3,47 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logoImg.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   // CODE
 
   return (
     <>
-      <Title>
-        <FiChevronLeft size={20} />
-        Minimalist Template Typescript React
-        <FiChevronRight size={20} />
-      </Title>
+      <img src={logoImg} alt="GitHub Explorer 2" />
+      <Title>Explore repositórios no GitHub</Title>
 
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42852997?s=460&u=80a399122290571393cce240b45a9c53035750f3&v=4"
+            alt="Eduardo Ribeiro"
+          />
+          <div>
+            <strong>dufellipeR/github_explorer2</strong>
+            <p>Lorem ipsum dolor firelight bluetooth</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42852997?s=460&u=80a399122290571393cce240b45a9c53035750f3&v=4"
+            alt="Eduardo Ribeiro"
+          />
+          <div>
+            <strong>dufellipeR/github_explorer2</strong>
+            <p>Lorem ipsum dolor firelight bluetooth</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+// import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  // CODE
+
+  return (
+    <>
+      <h1>Hello World </h1>
+    </>
+  );
+};
+
+export default Dashboard;
